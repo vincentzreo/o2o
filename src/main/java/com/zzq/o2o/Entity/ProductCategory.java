9fc4f1a36021669d18c5ac8addd0a8c4948fffa9
zhouzhangqi <1978892854@qq.com>
2019-06-18 18:00:11 +0800

    开始搭建框架结构

---
+++ src/main/java/com/zzq/o2o/Entity/ProductCategory.java
@@ -0,0 +1,55 @@
+package com.zzq.o2o.Entity;
+
+import java.util.Date;
+
+public class ProductCategory {
+    private Long productCategoryId;
+    //店铺id
+    private Long shopId;
+    //类别名称
+    private String productCategoryName;
+    //类别权重
+    private Integer priority;
+    //类别创建时间
+    private Date createTime;
+
+    public Long getProductCategoryId() {
+        return productCategoryId;
+    }
+
+    public void setProductCategoryId(Long productCategoryId) {
+        this.productCategoryId = productCategoryId;
+    }
+
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getProductCategoryName() {
+        return productCategoryName;
+    }
+
+    public void setProductCategoryName(String productCategoryName) {
+        this.productCategoryName = productCategoryName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/main/java/com/zzq/o2o/dao/AreaDao.java
@@ -0,0 +1,9 @@
+package com.zzq.o2o.dao;
+
+import com.zzq.o2o.Entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    List<Area> queryArea();
+}
